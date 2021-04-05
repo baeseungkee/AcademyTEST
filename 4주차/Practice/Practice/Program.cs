e6429d9be9f95a8ea4d6d0d28945f521411c5ea7
baeseungkee <tmdrl911226@anyractive.co.kr>
2021-04-05 17:41:02 +0900

    연습본 푸쉬

---
+++ 4주차/Practice/Practice/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            World world = new World();
+            world.Init();
+
+            foreach (Creature target in world.list)
+            {
+                if(target is iSwimmable)
+                {
+                    (target as iSwimmable).swim();
+                }
+            }
+        }
+        class World
+        {
+            public List<Creature> list = new List<Creature>();
+            public void Init()
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    list.Add(new Monkey());
+                }
+                for (int i = 0; i < 3; i++)
+                {
+                    list.Add(new Whale());
+                }
+                for (int i = 0; i < 3; i++)
+                {
+                    list.Add(new Dolphin());
+                }
+                for (int i = 0; i < 3; i++)
+                {
+                    list.Add(new Eagle());
+                }
+                for (int i = 0; i < 3; i++)
+                {
+                    list.Add(new Sparrow());
+                }
+                for (int i = 0; i < 3; i++)
+                {
+                    list.Add(new Pigeon());
+                }
+                for (int i = 0; i < 3; i++)
+                {
+                    list.Add(new GoldFish());
+                }
+                for (int i = 0; i < 3; i++)
+                {
+                    list.Add(new Eel());
+                }
+            }
+        }
+        interface iSwimmable
+        {
+            void swim();
+        }
+        abstract class Creature
+        {
+
+        }
+        abstract class Mammal : Creature
+        {
+
+        }
+        abstract class Bird : Creature
+        {
+
+        }
+        abstract class Fish : Creature
+        {
+
+        }
+        class Monkey : Mammal { }
+        class Whale : Mammal, iSwimmable
+        {
+            public void swim()
+            {
+                Console.WriteLine($"[{nameof(Whale)}]");
+            }
+        }
+        class Dolphin : Mammal { }
+        class Eagle : Bird { }
+        class Sparrow : Bird { }
+        class Pigeon : Bird { }
+        class Penguin : Bird, iSwimmable
+        {
+            void iSwimmable.swim()
+            {
+                throw new NotImplementedException();
+            }
+        }
+        class GoldFish : Fish, iSwimmable
+        {
+            void iSwimmable.swim()
+            {
+                throw new NotImplementedException();
+            }
+        }
+        class Eel : Fish, iSwimmable
+        {
+            void iSwimmable.swim()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ 4주차/Practice2/Practice2/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            World world = new World();
+            world.Init();
+
+            foreach (Creature creature in world.list)
+            {
+                if(creature is iSwimmable)
+                {
+                    (creature as iSwimmable).Swim();
+                }
+            }
+        }
+    }
+    class World
+    {
+        public List<Creature> list = new List<Creature>();
+        public void Init()
+        {
+            for (int i = 0; i<3; i++)
+            {
+                list.Add(new Monkey());
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                list.Add(new Whale());
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                list.Add(new Dolphin());
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                list.Add(new Eagle());
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                list.Add(new Sparrow());
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                list.Add(new Pigeon());
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                list.Add(new Penguin());
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                list.Add(new Goldfish());
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                list.Add(new Eel());
+            }
+        }
+    }
+    interface iSwimmable
+    {
+        void Swim();
+    }
+    abstract class Creature
+    {
+
+    }
+    abstract class Mammal : Creature
+    {
+
+    }
+    abstract class Birds : Creature
+    {
+
+    }
+    abstract class Fish : Creature
+    {
+
+    }
+    class Monkey : Mammal { }
+    class Whale : Mammal, iSwimmable
+    {
+        public void Swim()
+        {
+            Console.WriteLine($"[{nameof(Whale)}]");
+        }
+    }
+    class Dolphin : Mammal { }
+    class Eagle : Birds { }
+    class Sparrow : Birds { }
+    class Pigeon : Birds { }
+    class Penguin : Birds, iSwimmable
+    {
+        public void Swim()
+        {
+            Console.WriteLine($"[{nameof(Penguin)}]");
+        }
+    }
+    class Goldfish : Fish, iSwimmable
+    {
+        public void Swim()
+        {
+            Console.WriteLine($"[{nameof(Goldfish)}]");
+        }
+    }
+    class Eel : Fish, iSwimmable
+    {
+        public void Swim()
+        {
+            Console.WriteLine($"[{nameof(Eel)}]");
+        }
+    }
+}
